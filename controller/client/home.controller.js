e41aaf0e320587f26e672ce515ba0664ecbc3d4a
haocao <haotrungsondlna@gmail.com>
2026-06-28 08:53:42 +0700

    section4Home

--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -1,4 +1,5 @@
 const Tour = require("../../models/tour.model");
+const Category = require("../../models/category.model");
 const moment = require("moment");
 
 module.exports.home = async (req, res) => {
@@ -10,14 +11,40 @@ module.exports.home = async (req, res) => {
   }).sort({
     position: "desc"
   }).limit(6);
+  for (item of tourListSection2) {
+    item.departureDateFormat = moment(new Date(item.departureDate)).format("DD/MM/YYYY");
+  }
   // End section 2
 
-  for (item of tourListSection2) {
-    item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
+  // Section 4: Tour trong nước
+  const categorySection4 = await Category.findOne({ slug: "tour-trong-nuoc" });
+  const categoryIdSection4 = categorySection4._id.toString();
+  const listCategory = [categoryIdSection4];
+
+  const listSubCategory = await Category.find({
+    parent: categoryIdSection4,
+    deleted: false,
+    status: "active"
+  });
+
+  for (const item of listSubCategory) {
+    listCategory.push(item._id);
   }
+  
+  const tourListSection4 = await Tour.find({
+    category: {$in: listCategory},
+    deleted: false,
+    status: "active"
+  }).sort({ position: "desc" }).limit(8);
+
+  for (item of tourListSection4) {
+    item.departureDateFormat = moment(new Date(item.departureDate)).format("DD/MM/YYYY");
+  }
+  // End section 4: Tour trong nước
 
   res.render("client/pages/home.pug", {
     pageTitle: "Trang chủ",
-    tourListSection2: tourListSection2
+    tourListSection2: tourListSection2,
+    tourListSection4: tourListSection4
   });
 }
